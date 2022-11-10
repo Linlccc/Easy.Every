4b164f2ec8e134c1eaec71bfbf77c58e91fbca7d
Linlccc <1610450962@qq.com>
2022-11-10 18:01:41 +0800

    添加常量

--- Easy.Every.Infrastructure/Constant.cs
+++ Easy.Every.Infrastructure/Constant.cs
@@ -1,5 +1,39 @@
-﻿namespace Easy.Every.Infrastructure;
+﻿using System.Diagnostics;
+using System.Reflection;
 
-internal class Constant
+namespace Easy.Every.Infrastructure;
+
+/// <summary>
+/// 常量
+/// </summary>
+public static class Constant
 {
+    public const string EasyEvery = "Easy.Every";
+
+    /// <summary>
+    /// 应用程序文件名
+    /// </summary>
+    public const string ApplicationFileName = $"{EasyEvery}.exe";
+    /// <summary>
+    /// 程序目录
+    /// </summary>
+    public static readonly string ProgramDirectory = Directory.GetCurrentDirectory();
+    /// <summary>
+    /// 应用程序完整文件名
+    /// </summary>
+    public static readonly string ApplicationFileFullName = Path.Combine(ProgramDirectory, ApplicationFileName);
+    /// <summary>
+    /// 图片目录
+    /// </summary>
+    public static readonly string ImagesDirectory = Path.Combine(ProgramDirectory, "Images");
+
+    /// <summary>
+    /// 当前程序集
+    /// </summary>
+    public static readonly Assembly Assembly = typeof(Constant).Assembly;
+    /// <summary>
+    /// 版本
+    /// </summary>
+    public static readonly string Version = FileVersionInfo.GetVersionInfo(Assembly.Location).ProductVersion ?? "0.0.1";
+
 }
--- Easy.Every.Infrastructure/UserSettings/DataLocation.cs
+++ Easy.Every.Infrastructure/UserSettings/DataLocation.cs
@@ -3,7 +3,29 @@
 /// <summary>
 /// 数据位置
 /// </summary>
-public class DataLocation
+public static class DataLocation
 {
+    /// <summary>
+    /// 便携数据文件夹名
+    /// </summary>
+    public const string PortableDataFolderName = "UserData";
+    /// <summary>
+    /// 便携目录
+    /// </summary>
+    public static readonly string PortableDataDirectory = Path.Combine(Constant.ProgramDirectory, PortableDataFolderName);
+    /// <summary>
+    /// 本地数据目录
+    /// </summary>
+    public static readonly string LocalDataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Constant.EasyEvery);
 
+    /// <summary>
+    /// 数据目录
+    /// </summary>
+    public static string DataDirectory => IsUsePortableData() ? PortableDataDirectory : LocalDataDirectory;
+
+    /// <summary>
+    /// 是否使用便携数据
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsUsePortableData() => Directory.Exists(PortableDataDirectory);
 }
